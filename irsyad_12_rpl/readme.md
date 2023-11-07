74ecf966a7dbc767fc45e59a4e114a11c2df93f0
davSec01 <145743628+davSec01@users.noreply.github.com>
2023-11-07 14:49:25 +0700

    Add files via upload

---
+++ irsyad_12_rpl/readme.md
@@ -0,0 +1,6 @@
+Algoritma menghitung luas lingkaran
+
+menetapkan nilai Phi yaitu 3.14
+menghitung jari-jari lingkaran.
+menghitung luas lingkaran dengan rumus L=phi*r*r
+mencetak nilai L
---
+++ irsyad_12_rpl/script.js
@@ -0,0 +1,10 @@
+// Jari-jari lingkaran (ganti dengan nilai sesuai kebutuhan)
+let jariJari = parseFloat(Prompt(" masukan diameter")) ;
+let luas = parseFloat(Prompt("masukanJarijari"));
+
+// Hitung keliling lingkaran
+let keliling = 2 * Math.PI * jariJari;
+
+// Tampilkan hasil
+console.log("Luas lingkaran adalah: " + luas);
+console.log("Keliling lingkaran adalah: " + keliling);
